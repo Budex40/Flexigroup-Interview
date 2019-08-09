4386da6a863b09838e7a2ae62534c6f8077f6ea3
TALABI OLABODE FOLORUNSO <30889918+SosthenesAlpha@users.noreply.github.com>
2019-08-09 13:07:08 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+      show: false,
+      items: [
+        "Development",
+        "Call me",
+        "Hire Me",
+        "Check up on me",
+        "My portfolio",
+        "Scale"
+      ]
+    }
+  })
